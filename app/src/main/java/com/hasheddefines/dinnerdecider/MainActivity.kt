5e414e3de6acd5d276e421fbf55853c9917113b2
Matt Linaberry <mtlinaberry@gmail.com>
2017-09-09 23:37:36 -0400

    Now really thru lecture 8

--- app/src/main/java/com/hasheddefines/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/hasheddefines/dinnerdecider/MainActivity.kt
@@ -3,16 +3,28 @@ package com.hasheddefines.dinnerdecider
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
+    val foodList = arrayListOf("Chinese", "Hamburgers", "Pizza", "McDonald's", "Barro's Pizza")
 
     // viewDidLoad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  // link the main activity view here.
 
-        decideButton.setOnClickListener {  }
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFoodIndex = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFoodIndex]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+            println(foodList)
+        }
     }
 }
